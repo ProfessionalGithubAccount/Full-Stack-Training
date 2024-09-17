1f6824af0363388b5d75f92c519b497d1f3c33de
ProfessionalGithubAccount <KausthubRao.Garikapati@ust.com>
2024-09-17 14:47:18 +0530

    Add files via upload

---
+++ Jpaone2many/src/main/java/com/ust/jparelationships/Jpaone2manyApplication.java
@@ -0,0 +1,13 @@
+package com.ust.jparelationships;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jpaone2manyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jpaone2manyApplication.class, args);
+	}
+
+}
